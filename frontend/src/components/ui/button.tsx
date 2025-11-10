6818e5d6a02f24649a17bd26e55d9f660001b77f
Ltcbe <support@terminalcommun.be>
2025-11-10 09:46:28 +0100

    Delete frontend directory

--- frontend/src/components/ui/button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & { variant?: 'default' | 'outline' }
-export const Button: React.FC<Props> = ({ variant='default', className='', ...props }) => {
-  const base = 'input'
-  const style = variant === 'outline' ? '' : ' primary'
-  return <button {...props} className={base + style + (className ? ' ' + className : '')} />
-}
